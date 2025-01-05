2ee5ea73495e7cf76f0735614cffec680d24e5b0
webdevyoo <yjk7454@naver.com>
2025-01-06 01:11:26 +0900

    feat: Section02. 타입스크립트 컴파일 옵션 - include 옵션
    tsconfig.json include옵션 src 디렉토리 설정 및 tsc 전체 컴파일 진행

--- section01/src/index.js
+++ section01/src/index.js
@@ -1,2 +1,3 @@
 console.log("Hello TypeScript");
 var a = 1;
+console.log(typeof a);
--- section01/src/index.ts
+++ section01/src/index.ts
@@ -1,2 +1,3 @@
-console.log("Hello TypeScript")
+console.log("Hello TypeScript");
 const a: number = 1;
+console.log(typeof a)
