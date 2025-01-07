91ad46e0209824e294f6a5554994dd4bd838f31b
webdevyoo <yjk7454@naver.com>
2025-01-08 06:05:24 +0900

    feat: `Section02 타입스크립트 컴파일러 옵션 설정` - OutDir 옵션 dist 설정 및 tsc 컴파일 진행

---
+++ section01/dist/target.js
@@ -0,0 +1,8 @@
+/**
+ * [ES5]
+ * var func = function () { return console.log("hello"); };
+ * [ESNext]
+ * const func = () => console.log("hello");
+ * @returns
+ */
+const func = () => console.log("hello");
