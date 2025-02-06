f3b9d95413e38baa15c455d1def0ec337f48ed2d
webdevyoo <yjk7454@naver.com>
2025-02-07 00:34:35 +0900

    feat: Section05 - 타입스크립트 프로젝트 초기 테스트 코드 컴파일 테스트 진행

---
+++ section04/dist/index.js
@@ -0,0 +1,8 @@
+/**
+ * 타입스크립트 프로젝트 초기 테스트
+ * 1. tsc
+ * 2. node dist/index.js
+ * 3. tsx src/index.ts
+ */
+console.log("Hello New Project");
+export {};
--- section04/src/index.ts
+++ section04/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * 타입스크립트 프로젝트 초기 테스트
  * 1. tsc
- * 2. node dist/index.js
+ * 2. node dist/index.js - 현재 버전에서는 안되므로 tsx로 빌드/컴파일 동시 진행
  * 3. tsx src/index.ts
  */
 console.log("Hello New Project")
