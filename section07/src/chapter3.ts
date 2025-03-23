01d28ab8136be0bb48a7284d1ede79c405067e2b
webdevyoo <yjk7454@naver.com>
2025-03-23 14:16:34 +0900

    feat: Section08 - 제네릭 인터페이스 활용 예시, 사용자 관리 프로그램 예제코드 추가

--- section07/src/chapter3.ts
+++ section07/src/chapter3.ts
@@ -90,3 +90,110 @@ type IMap2<V> = {
 let stringMap2: IMap2<string> = {
   key: "hello"
 }
+/* 
+### 제네릭 인터페이스의 활용 예시
+
+#### 사용자(User) 관리 프로그램 예제
+사용자 구분: 학생 / 개발자
+
+##### 1. 학생 사용자 인터페이스 정의
+'student' 문자열 리터럴 타입을 갖는 type 프로퍼티와, string 타입을 갖는 school프로퍼티를 정의한다.
+*/
+interface IStudent {
+  type: 'student';
+  school: string;
+}
+/* 
+##### 2. 개발자 사용자 인터페이스 정의
+'developer' 문자열 리터럴 타입을 갖는 type 프로퍼티와, string 타입을 갖는 skill프로퍼티를 정의한다.
+*/
+interface IDeveloper {
+  type: 'developer';
+  skill: string;
+
+}
+/* 
+IStudent 인터페이스의 type 프로퍼의 string 리터럴 타입 'student'와 IDeveloper 인터페이스의 type 프로퍼의 string 리터럴 타입 'developer' 각각을 union으로 묶으면 서로소 union 타입이 된다.  
+이러한 서로소 유니온 타입은 타입을 좁힐때 유용할것이다.  
+*/
+
+/* 
+다음으로 학생과 개발자 모두를 아우르는 User 타입 인터페이스를 만들어 본다.  
+모든 사용자는 공통적으로 이름을 갖기 때문에 name 프로퍼티를 추가하고, 학생인지 개발자인지를 구분할수 있도록 IStudent | IDeveloper 유니온 타입을 갖는 profile 프로퍼티를 추가한다.
+*/
+
+interface IUser {
+  name: string;
+  profile: IStudent | IDeveloper
+}
+/* 
+정의한 IUser인터페이스를 타입으로 갖는 개발자 변수와 학생 변수를 각각 선언하여 값을 할당해 본다.  
+*/
+const developerUser: IUser = {
+  name: '유혁스쿨',
+  profile: {
+    type: 'developer',
+    skill: 'TypeScript'
+  }
+}
+
+const studentUser: IUser = {
+  name: '유혁스쿨',
+  profile: {
+    type: 'student',
+    school: '학은제..'
+  }
+}
+/* 
+학생만 사용가능한 기능 `등교`를 함수로 정의한다.  
+IUser인터페이스의 profile 프로퍼티에 현재 학생 사용자만 특정할 수 있는 타입을 만들어 놓지 않았으므로 Iuser타입으로 매개변수를 정의한 뒤, 조건문을 이용해서 타입을 좁히도록 한다.  
+개발자 사용자가 들어왔을 경우 잘못오셨다를 출력하고 함수를 종료시키고, 학생 사용자가 들어왔을 때만 등교완료 로그를 출력하도록 구현한다.
+만약 우리가 관리하는 프로그램이 계속해서 사용자 구분도 많아지고, 특정 회원만 이용할 수 있는 함수도 많아진다면 함수를 만들 때마다 타입 좁히기를 써야하기 때문에 조건문을 계속 만들어야 해서 굉장히 불편해질것이다.  
+이런 경우 제너릭 인터페이스를 사용할 경우 훨씬 깔끔하게 코드를 작성할 수 있다.  
+*/
+function goToSchool(user: IUser) {
+  if (user.profile.type !== 'student') {
+    console.log('잘 못 오셨습니다.');
+    return;
+  }
+  const school = user.profile.school;
+  console.log(`${school}로 등교 완료`);
+
+}
+
+/* 
+User 인터페이스를 제네릭인터페이스로 구현
+*/
+interface IUserB<T> {
+  name: string;
+  profile: T
+}
+const developerUserB: IUserB<IDeveloper> = {
+  name: '유혁스쿨',
+  profile: {
+    type: 'developer',
+    skill: 'TypeScript'
+  }
+}
+
+const studentUserB: IUserB<IStudent> = {
+  name: '유혁스쿨',
+  profile: {
+    type: 'student',
+    school: '학은제..'
+  }
+}
+
+/* 
+제네릭 인터페이스는 변수의 타입을 정의함과 동시에 타입 변수에 할당할 타입을 직접 명시해줘야하기 때문에 user 매개변수의 타입 IUserB의 타입변수에 IStudent를, 개발자 사용자 변수의 타입변수에는 IDeveloper를, 학생 사용자 변수의 타입변수에는 IStudent를 할당한다.  
+goToSchoolB() 같은 함수의 매개변수의 타입은 IUserB타입인데, 타입변수 T에 IStudent를 할당했기 때문에 profile의 타입변수 T에는 IStudent 인터페이스가 할당될것이고, 해당 인터페이스의 type은 문자열 리터럴 타입인 'student'가 될것이다.  
+따라서, goToSchoolB 함수에는 제너릭타입 IDeveloper의 IUserB가 들어올 수 없기 때문에 앞서 구현한 타입 좁히기 조건문을 삭제해도 된다.
+*/
+function goToSchoolB(user: IUserB<IStudent>) {
+  const school = user.profile.school;
+  console.log(`${school}로 등교 완료`);
+}
+goToSchoolB(developerUserB) // [Error] Argument of type 'IUserB<IDeveloper>' is not assignable to parameter of type 'IUserB<IStudent>'.  Property 'school' is missing in type 'IDeveloper' but required in type 'IStudent'.ts(2345)  
+goToSchoolB(studentUserB)
+
+
