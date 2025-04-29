11f146edb4a6bcee829e7c114f31fd09f6aca2d9
webdevyoo <yjk7454@naver.com>
2025-04-30 02:25:37 +0900

    feat: `Section12` - App.tsx React Input 태그 ChangeEvent 타입스크립트 Reac표준 타입 적용

--- section12/src/App.tsx
+++ section12/src/App.tsx
@@ -3,9 +3,17 @@ import './App.css';
 
 function App() {
   const [text, setText] = useState<string>("");
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
   return (
     <div className="App">
       <h1>Todo</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={onChangeInput}
+      />
     </div>
   );
 }
