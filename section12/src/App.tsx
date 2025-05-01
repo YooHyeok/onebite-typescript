b13161c8f7bd150725f86ad6b66740ff5864d1c3
YooHyeok <yjk7454@naver.com>
2025-05-02 01:18:14 +0900

    feat: `Section12` - todos state 추가 및 Todo 인터페이스 타입 정의 및 타입 지정, 추가 버튼 태그 추가

--- section12/src/App.tsx
+++ section12/src/App.tsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
 
+interface Todo {
+  id: number;
+  content: string;
+}
 function App() {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
   const [text, setText] = useState<string>("");
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onChange_Input = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value)
   }
+
   return (
     <div className="App">
       <h1>Todo</h1>
       <input
         type="text"
         value={text}
-        onChange={onChangeInput}
+        onChange={onChange_Input}
       />
+      <button>추가</button>
     </div>
   );
 }
