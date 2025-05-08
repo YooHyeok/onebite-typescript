59ca737e3e3951d122e523ff7fc4d402516eb58c
webdevyoo <yjk7454@naver.com>
2025-05-09 00:27:01 +0900

    feat: `Section12` - TODO 삭제기능 구현 TodoItem.tsx Props 타입 삭제 함수 타입 정의 및 버튼태그 바인딩

--- section12/src/components/TodoItem.tsx
+++ section12/src/components/TodoItem.tsx
@@ -2,10 +2,16 @@
 
 interface Props extends Todo{
   // extra: string; // 추가적인 새로운 props 요소를 받을 수 있음.
+  onClickDelete: (id: number) => void;
 }
-export default function TodoItem({id, content}: Props) {
+export default function TodoItem({id, content, onClickDelete}: Props) {
+
+  const onClickButton = () => {
+    onClickDelete(id);
+  }
+
   return <div> 
     {id} 번: { content }
-    <button>삭제</button>
+    <button onClick={onClickButton}>삭제</button>
   </div>
 }
